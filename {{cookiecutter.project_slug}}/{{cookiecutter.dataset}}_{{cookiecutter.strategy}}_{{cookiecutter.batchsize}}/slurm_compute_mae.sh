804d42516f74f3583b6551846a0f212e3a548c53
Kunal Ghosh <kunalghosh@users.noreply.github.com>
2022-03-14 19:01:05 +0200

    Fixing bugs in MAE computation script.

--- {{cookiecutter.project_slug}}/{{cookiecutter.dataset}}_{{cookiecutter.strategy}}_{{cookiecutter.batchsize}}/slurm_compute_mae.sh
+++ {{cookiecutter.project_slug}}/{{cookiecutter.dataset}}_{{cookiecutter.strategy}}_{{cookiecutter.batchsize}}/slurm_compute_mae.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name={{cookiecutter.dataset}}_{{cookiecutter.strategy}}_{{cookiecutter.batchsize}}_MAE
 #SBATCH --account=project_2000382
-#SBATCH --time=02:00:00
+#SBATCH --time=00:30:00
 #SBATCH --mem-per-cpu=5G
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=18
+#SBATCH --cpus-per-task=32
 #SBATCH --partition=small
 #SBATCH --array=3
 #SBATCH --output=activeLearning_1k_longrun_test_mae_run1_%a.out
@@ -12,10 +12,21 @@
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 dirs=(
+"0"
+"1"
+"2"
+"3"
+"4"
+"5"
+"6"
+"7"
+"8"
+"9"
+"10"
+"11"
+"12"
 "13"
 "14"
-"15"
-"16"
 )
 
 module load python-data/3.7.6-1
@@ -30,4 +41,4 @@ export PROJAPPL=/projappl/project_2000382
 # cd ${dirs[$SLURM_ARRAY_TASK_ID]}
 # srun ../main.sh $(cut -d / -f3 "${dirs[$SLURM_ARRAY_TASK_ID]}") input.dat
 # run_num=`cut -d / -f10 | pwd`
-srun run_getmae.sh ${dirs[$SLURM_ARRAY_TASK_ID] }
+srun run_getmae.sh ${dirs[$SLURM_ARRAY_TASK_ID]} {{cookiecutter.strategy}} {{cookiecutter.batchsize}} {{cookiecutter.dataset}}
